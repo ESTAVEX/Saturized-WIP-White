3471bed79a020d4554c029fbbc9d06250c665b14
Milan Obrenović <mobrenovic@smart.edu.rs>
2016-05-30 12:42:25 +0200

    Fixed image stretching, fixed nav height, rolled back custom scrolling

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,33 @@
 
-var swiper = new Swiper('.swiper-container', {
+$(document).ready(function() {
 
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    paginationBulletRender: function (index, className) {
-        return '<span class="' + className + '"><span class="pagination-number">' + (index + 1) + '</span></span>';
-    },
-    direction: 'vertical',
-    slidesPerView: 1,
-    mousewheelControl: true,
-    keyboardControl: true,
-    height: $(window).height(),
-    autoHeight: true
+    var swiper = new Swiper('.swiper-container', {
+
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        paginationBulletRender: function (index, className) {
+            return '<span class="' + className + '"><span class="pagination-number">' + (index + 1) + '</span></span>';
+        },
+        direction: 'vertical',
+        slidesPerView: 1,
+        mousewheelControl: true,
+        keyboardControl: true,
+        height: $(window).height(),
+        calculateHeight: true,
+        autoHeight: true
+
+    });
+
+    var open = true;
+    $("#navigation-wrapper i.toggle-btn").click(function(){
+        if (open)
+            $('#navigation-wrapper .menu-list').css('padding', '40px');
+        else
+            $('#navigation-wrapper .menu-list').css('padding', '0px');
+
+        open = !open;
+    });
+
+    $('.imgLiquidFill').imgLiquid();
 
 });
